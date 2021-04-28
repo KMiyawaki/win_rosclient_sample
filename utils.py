815dd672fbf9162166223a4ffbc58b106a281f3a
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2021-04-28 09:58:54 +0900

    remove: meaningless class SimpleTimer

--- utils.py
+++ utils.py
@@ -4,27 +4,6 @@ import bson
 import math
 import time
 
-
-class SimpleTimer(object):
-    def __init__(self, sec=0, hz=1):
-        self.set_time(sec)
-        self.period = 1.0 / hz
-
-    def set_time(self, sec):
-        self.tm = sec
-
-    def elapsed(self, sec):
-        return sec - self.tm
-
-    def check_elapsed(self, sec, set_if_elapsed=True):
-        elapsed = self.elapsed(sec)
-        if elapsed > self.period:
-            if set_if_elapsed:
-                self.set_time(sec)
-            return (True, elapsed)
-        return (False, elapsed)
-
-
 def build_ros_header(time, frame_id, seq):
     s2, s = math.modf(time)
     return {
